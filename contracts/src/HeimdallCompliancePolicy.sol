3b934f9b39375386ab339f07e9542425981e7fdb
Udit <udit@eskaytech.com>
2023-09-04 00:47:57 +0530

    Update compliance policy contract

--- contracts/src/HeimdallCompliancePolicy.sol
+++ contracts/src/HeimdallCompliancePolicy.sol
@@ -3,29 +3,62 @@ pragma solidity ^0.8.0;
 
 import './interfaces/IHeimdallCompliancePolicy.sol';
 
-contract CompliancePolicy is IHeimdallCompliancePolicy {
+contract HeimdallCompliancePolicy is IHeimdallCompliancePolicy {
 	string public name;
 	string public version;
-	address public policyCreator;
-	ComplianceRequirements public complianceRequirements;
 
-	constructor(string memory _name, string memory _version, bool _blockOfacAssets) {
+	mapping(ComplianceRequirement => bool) public requirements;
+
+	constructor(
+		string memory _name,
+		string memory _version,
+		ComplianceRequirement[] memory _requirements,
+		bool[] memory _isActive
+	) {
 		require(bytes(_name).length > 0, 'Name must not be empty');
 		require(bytes(_version).length > 0, 'Version must not be empty');
-		require(
-			_blockOfacAssets == true || _blockOfacAssets == false,
-			'BlockOfacAssets must be a boolean'
-		);
+		require(_requirements.length == _isActive.length, 'Input arrays length mismatch');
+
+		for (uint256 i = 0; i < _requirements.length; i++) {
+			require(
+				uint256(_requirements[i]) <= uint256(ComplianceRequirement.BlockOfacAssets),
+				'Invalid Compliance Requirement'
+			);
+			requirements[_requirements[i]] = _isActive[i];
+		}
 
 		name = _name;
 		version = _version;
-		policyCreator = msg.sender;
-
-		complianceRequirements = ComplianceRequirements({blockOfacAssets: _blockOfacAssets});
 	}
 
-	// Getter function to check the value of blockOfacAssets
-	function isBlockOfacAssetsEnabled() external view returns (bool) {
-		return complianceRequirements.blockOfacAssets;
+	// Function to check the state of a ComplianceRequirement
+	function isRequirement(ComplianceRequirement _requirement) external view returns (bool) {
+		return requirements[_requirement];
+	}
+
+	// Function to get all ComplianceRequirements that are true
+	function getRequirements() external view returns (ComplianceRequirement[] memory) {
+		uint256 trueCount = 0;
+
+		// Loop through all ComplianceRequirements
+		for (uint256 i = 0; i <= uint256(ComplianceRequirement.BlockOfacAssets); i++) {
+			if (requirements[ComplianceRequirement(i)]) {
+				trueCount++;
+			}
+		}
+
+		// Create an array to store the true ComplianceRequirements
+		ComplianceRequirement[] memory trueRequirements = new ComplianceRequirement[](trueCount);
+		uint256 index = 0;
+
+		// Populate the trueRequirements array
+		for (uint256 i = 0; i <= uint256(ComplianceRequirement.BlockOfacAssets); i++) {
+			if (requirements[ComplianceRequirement(i)]) {
+				trueRequirements[index] = ComplianceRequirement(i);
+				index++;
+			}
+		}
+
+		return trueRequirements;
 	}
 }
--- contracts/test/01_compliancePolicy.ts
+++ contracts/test/01_compliancePolicy.ts
@@ -1,18 +1,36 @@
 import hre from 'hardhat'
 import { expect } from 'chai'
-import { CompliancePolicy } from '../typechain-types'
+import { HeimdallCompliancePolicy } from '../typechain-types'
 
-describe('CompliancePolicy', function () {
-	let compliancePolicy: CompliancePolicy
+enum ComplianceRequirement {
+	BlockOfacAssets = 0
+}
+
+describe('HeimdallCompliancePolicy', function () {
+	let compliancePolicy: HeimdallCompliancePolicy
 
 	beforeEach(async () => {
-		const contractFactory = await hre.ethers.getContractFactory('CompliancePolicy')
-		compliancePolicy = await contractFactory.deploy('Sample Policy', '1.0', true)
+		const contractFactory = await hre.ethers.getContractFactory('HeimdallCompliancePolicy')
+		compliancePolicy = await contractFactory.deploy(
+			'Sample Policy',
+			'1.0.0',
+			[ComplianceRequirement.BlockOfacAssets], // Index value for the enum
+			[true]
+		)
 	})
 
-	it('should have the correct initial values', async function () {
+	it('should set the name and version correctly', async () => {
 		expect(await compliancePolicy.name()).to.equal('Sample Policy')
-		expect(await compliancePolicy.version()).to.equal('1.0')
-		expect(await compliancePolicy.isBlockOfacAssetsEnabled()).to.equal(true)
+		expect(await compliancePolicy.version()).to.equal('1.0.0')
+	})
+
+	it('should set compliance requirements and return their states', async () => {
+		expect(await compliancePolicy.isRequirement(ComplianceRequirement.BlockOfacAssets)).to.be.true
+	})
+
+	it('should get all true compliance requirements', async () => {
+		const trueRequirements = await compliancePolicy.getRequirements()
+		expect(trueRequirements).to.have.lengthOf(1)
+		expect(trueRequirements[0]).to.equal(ComplianceRequirement.BlockOfacAssets)
 	})
 })
